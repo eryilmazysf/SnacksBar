47280ce8afb8140aed9d568affb46a0fbd9118fc
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-05-28 00:25:19 -0400

    last version

--- myScript.js
+++ myScript.js
@@ -1,9 +1,17 @@
 buttonsA = document.querySelectorAll("#a");
 buttonsB = document.querySelectorAll("#b");
 buttonsC = document.querySelectorAll("#c");
+returnMoney = document.getElementById("returnMoney");
+totalMoney = document.getElementById("total");
+showMoney = document.getElementById("showMoney"); //shows the money put in the box
+bought = document.getElementById("bought");
+money = document.getElementById("money");
+add = document.getElementById("add");
 
+//money boxes
 let total = [];
 
+// access to each buttons
 for (const buttonA of buttonsA) {
   buttonA.addEventListener("click", function () {
     let getTotal = calculateTotal();
@@ -11,17 +19,15 @@ for (const buttonA of buttonsA) {
     let returnPrice = (getTotal - priceA).toFixed(2);
     //console.log(getTotal);
     if (getTotal >= priceA) {
-      document.getElementById("returnPrice").innerText = returnPrice;
-      total = [Number(returnPrice)];
-      document.getElementById("total").innerText = returnPrice;
-      document.getElementById("showMoney").innerText = 0;
-      document.getElementById(
-        "bought"
-      ).innerText = `You bought : ${buttonA.innerText}`;
+      returnMoney.innerText = returnPrice;
+      total = [Number(returnPrice)]; // first element of total array will be remaning money after buying a snack
+      totalMoney.innerText = returnPrice; // Total Screen will be remaining money after buying a snack
+      showMoney.innerText = 0;
+      bought.innerText = `You bought : ${buttonA.innerText}`; //show the snack buy by client
     } else {
-      document.getElementById("returnPrice").setAttribute("style", "color:red");
-      document.getElementById("returnPrice").innerText =
-        "you do not enough money to buy this product";
+      //insufficient fund to buy a snack
+      returnMoney.setAttribute("style", "color:red");
+      returnMoney.innerText = "you do not enough money to buy this product";
     }
   });
 }
@@ -32,17 +38,14 @@ for (const buttonB of buttonsB) {
     let returnPrice = (getTotal - priceB).toFixed(2);
 
     if (getTotal >= priceB) {
-      document.getElementById("returnPrice").innerText = returnPrice;
+      returnMoney.innerText = returnPrice;
       total = [Number(returnPrice)];
-      document.getElementById("total").innerText = returnPrice;
-      document.getElementById("showMoney").innerText = 0;
-      document.getElementById(
-        "bought"
-      ).innerText = `You bought : ${buttonB.innerText}`;
+      totalMoney.innerText = returnPrice;
+      showMoney.innerText = 0;
+      bought.innerText = `You bought : ${buttonB.innerText}`;
     } else {
-      document.getElementById("returnPrice").setAttribute("style", "color:red");
-      document.getElementById("returnPrice").innerText =
-        "you do not enough money to buy this product";
+      returnMoney.setAttribute("style", "color:red");
+      returnMoney.innerText = "you do not enough money to buy this product";
     }
   });
 }
@@ -53,51 +56,57 @@ for (const buttonC of buttonsC) {
     let returnPrice = (getTotal - priceC).toFixed(2);
 
     if (getTotal >= priceC) {
-      document.getElementById("returnPrice").innerText = returnPrice;
+      returnMoney.innerText = returnPrice;
       total = [Number(returnPrice)];
-      document.getElementById("total").innerText = returnPrice;
-      document.getElementById("showMoney").innerText = 0;
-      document.getElementById(
-        "bought"
-      ).innerText = `You bought : ${buttonC.innerText}`;
+      totalMoney.innerText = returnPrice;
+      showMoney.innerText = 0;
+      bought.innerText = `You bought : ${buttonC.innerText}`;
     } else {
-      document.getElementById("returnPrice").setAttribute("style", "color:red");
-      document.getElementById("returnPrice").innerText =
-        "you do not enough money to buy this product";
+      returnMoney.setAttribute("style", "color:red");
+      returnMoney.innerText = "you do not enough money to buy this product";
     }
   });
 }
 
-money = document.getElementById("money");
-add = document.getElementById("add");
 add.addEventListener("click", function () {
   //console.log(money.value);
+  // accept just 0.25 or 0.1 or 0.05 cent moneys
   if (
     money.value == String(0.25) ||
     money.value == String(0.1) ||
     money.value == String(0.05)
   ) {
-    document.getElementById("showMoney").setAttribute("style", "color:green");
-    document.getElementById("showMoney").innerText = `${money.value}`;
-    total.push(Number(money.value));
+    showMoney.setAttribute("style", "color:green");
+    showMoney.innerText = `${money.value}`;
+    total.push(Number(money.value)); // add each  money to the money box
     calculateTotal();
+    returnMoney.innerText = "";
+    bought.innerText = "";
   } else {
-    document.getElementById("showMoney").setAttribute("style", "color:red");
-    document.getElementById("showMoney").innerText =
-      "you can put $0.05 or $0.25 or $0.1";
+    //don't accept except of 0.25 or 0.1 or 0.05 moneys
+    returnMoney.setAttribute("style", "color:red");
+    returnMoney.innerText = "you can put $0.05 or $0.25 or $0.1";
   }
   money.value = "";
 });
+
+//calculate the sum of the coins in the box
 function calculateTotal() {
   //console.log("total", total);
   let sum = total.reduce(function (a, b) {
     return a + b;
   }, 0);
   //console.log("sum", sum);
-  document.getElementById("total").innerText = sum;
+  totalMoney.innerText = sum;
   return sum;
 }
-
+//if the client cancel buying snack
 document.getElementById("cancel").addEventListener("click", function () {
-  window.location.reload();
+  returnMoney = Number(totalMoney.innerText);
+  if (returnMoney > 0) {
+    returnMoney.innerText = calculateTotal();
+    total = [0];
+    totalMoney.innerText = 0;
+    showMoney.innerText = 0;
+  }
 });
